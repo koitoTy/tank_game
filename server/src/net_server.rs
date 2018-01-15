95cf4c45ff77d40e48a8b64c559248474b235d66
koitoTyan <34410977+koitoTyan@users.noreply.github.com>
2018-01-15 03:50:37 +0300

    Add files via upload
    Переделываю сервер т.к. нет необходимости такого сложного кода, как раньше в методе main

--- server/src/net_server.rs
+++ server/src/net_server.rs
@@ -13,7 +13,18 @@ use std::fs::File;
 use std::time::Duration;
 use std::mem;
 
-
+struct Connect{
+addr: TcpStream,
+id: u32,
+}
+trait con {
+fn new(stream: TcpStream, id: u32)->Connect;
+}
+impl con for Connect{
+ fn new(stream: TcpStream, id: u32)->Connect{
+	Connect{addr:stream, id:id}
+}
+}
 pub fn start_game(){
 	
    }
@@ -71,56 +82,43 @@ pub fn start_game_handle(){
 	println!("[Запускаю сервер!]");
 	let listener = TcpListener::bind(ip_port.as_str()).unwrap();	
 	println!("{:?}", listener);
-	let (sender, receiver) = mpsc::channel::<String>();
+	let (sender, receiver) = mpsc::channel();
 	//let(sen_, recv_) = mpsc::channel();
-	
+
+	let mut Connects:Vec<Connect> = Vec::new();
+	let mut k = 0;
 	for i in 0..number_player {
 		//принимаем каждого последовательно
 	println!("Принимаю клиента номер:[{}]", i+1);
 	match listener.accept(){
-		Ok((mut stream, addr)) => { 				
-			addrs.push(addr);	
+		Ok((mut stream, addr)) => { 
 			let sender_clone = mpsc::Sender::clone(&sender);
-			let id = addrs.len() - 1;
-			thread::spawn(move ||{
-				//let id = id;
-				/* тут мы передаём все данные */
-				thread::spawn(move || { 
-						/*а тут читаем данные*/
-					let mut buf:[u8; 256] = [0; 256];
-					let q:[u8; 8] = [0; 8]; 
-					/* 	read	 */
-					loop {
-						stream.read(&mut buf).is_ok(); // -> bool
-						if buf.starts_with(&q) == false { 
-							sender_clone.send(String::from_utf8(buf.to_vec()).unwrap()).unwrap(); 
-						}
-					}
-					/*receiver*/
-				}); 
-				loop{					
-					/*
-						receiver
-							sender
-					*/
-				
-				}	
-			});
+			let (send_, recv_) = mpsc::channel();
+			thread::spawn(move|| { 
+				{send_.send(stream.try_clone().expect("Клиент упал..")).unwrap();}
+				let q:[u8;8] = [0;8];
+				let mut buf:[u8; 256] = [0; 256];
+				println!("Принимаем [{}]", k);
+				loop { 
+					stream.read(&mut buf);
+					if buf.starts_with(&q) == false { sender_clone.send(String::from_utf8(buf.to_vec()).unwrap()).unwrap(); }
+				 }
+			 });
+			{
+			addrs.push(addr);	
+			let s = recv_.recv().unwrap();
+			let a = Connect::new(s, k);
+			Connects.push(a);		
+			k+=1;
+			}
 		},
 		Err(e) => {  },
 	}}	
-	for item in receiver {
-		/* если время ожидание истекло и сервер запущен - > говорим адреса*/	
-		// if start == true {
-			/*send_.send(item).unwrap();
-			thread::spawn(move ||{
-				for it in recv_{
-					
-				}
-			});*/
-		// }
-	}
+		for item in receiver{
+			println!("Приняли сообщение [{}]", item);
+		}
    }
+
 pub fn slim(arg1: String, arg2: char)->String{
 
 	let mut arg_return:String = String::new();
