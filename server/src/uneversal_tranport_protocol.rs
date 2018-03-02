f7e7dec53acfc8e0e7c39fb6da6539bc280ed286
koitoTyan <34410977+koitoTyan@users.noreply.github.com>
2018-03-02 23:38:00 +0300

    Update uneversal_tranport_protocol.rs

--- server/src/uneversal_tranport_protocol.rs
+++ server/src/uneversal_tranport_protocol.rs
@@ -16,6 +16,7 @@ pub struct utp{
 	ip: String,/*порт и ip конечного компа (клиента)*/
 	time: u64,
 	part: u8,
+    data: data,
 }
 
 
@@ -42,9 +43,7 @@ impl data{
 	fn buf(&self)->[u8; 9]{
 		self.data
 	}
-	fn drop(self){
-		// уничтожаем
-	}
+	fn drop(self){}
 }
 
 
@@ -156,7 +155,8 @@ impl utp{
 		let port: &str = &*self.port.to_string();
 		let wait_time = self.time;
 	    let part = self.part;
-		
+		let mut b_q: [u8; 7] = [0; 7];
+        
 		let mut buf: [u8; 9] = [0; 9];// mes_type, id_mes, id EvType x y z d , control byte (1010)
 			
 
@@ -175,10 +175,12 @@ impl utp{
 	for i in 0..part{
 		let a = socket.send(&data).is_ok();		
 		thread::sleep(Duration::from_millis(1));
-		if y == false {
-		 	let ok_q = socket.recv(&mut buf).is_ok(); // чтобы паники небыло
-				if buf[8] == 1010 { y = true;}
-		}	
+        if buf.starts_with(&b_q)==false{break;} else{
+            if y == false {
+                let ok_q = socket.recv(&mut buf).is_ok(); // чтобы паники небыло
+                    if buf[8] == 1010 { y = true;}
+            }	
+        }
 	}	if y == false { return data{connect: false, data: [0; 9], err_code: 404, err_text: "bad connect!".to_string()}; }
 		 data{connect: true, data: buf, err_code: 0, err_text: "all ok".to_string()}		
     }
@@ -186,15 +188,10 @@ impl utp{
 		utp{port: 0, ip: "".to_string(), time: 1, part: 10}
 	}
 	
-/*
-struct utp{
-	port: u16,
-	ip: String,
-}
-*/
 }
    pub fn new(port: u16, ip: String)->utp{
-		utp{ port: port, ip: ip, time: 1, part: 10 }		
+		utp{ port: port, ip: ip, time: 1, part: 10, 
+            data: data{connect: false, data: [0; 9], err_code: 404, err_text: "bad connect!".to_string()} }		
 	}
    pub fn drop(a: utp){}
 }
