c9a2eb5c01623fcbca5a44e9f32c3b1a1b78352a
koitoTyan <34410977+koitoTyan@users.noreply.github.com>
2018-03-07 10:44:11 +0300

    Доделала .send() метод

--- Tank_ver.alpha/Tank_v0.1_alpha/uneversal_tranport_protocol.cs
+++ Tank_ver.alpha/Tank_v0.1_alpha/uneversal_tranport_protocol.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace UnTP_CSharp
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
         {
@@ -14,28 +14,57 @@ namespace UnTP_CSharp
         }
     }
 
-    class utp {
+    class utp
+    {
         private int port { get; set; }
-        private string ip{ get; set; }
+        private string ip { get; set; }
         private data data { get; set; }
-        public utp(int port_, string ip_) { port = port_; ip = ip_; data = new data(false, new int[9], 404, "bad connect!"); }       
 
-        internal void send(){
-            
-        }
+        public utp(int port_, string ip_) { port = port_; ip = ip_; data = new data(false, new int[9], 404, "bad connect!"); }
+
+        internal void send()
+        {
+            int[] buf = new int[9];
+
+            UdpClient socket = new UdpClient();
+            IPEndPoint IpPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+             try
+            {               
+
+                // Преобразуем данные в массив байтов
+                byte[] bytes = data.data_;
+
+                // Отправляем данные
+                socket.Send(bytes, bytes.Length, IpPoint);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            finally
+            {
+                // Закрыть соединение
+                //socket.Close();
+            }
         
-        internal void recv(){
-            
+        }
+
+        internal void recv()
+        {
+
         }
     }
-    class data {
-        private bool connect { get; set; }
-        private int[] data_ { get; set; }
-        private int ErrCode { get; set; }
-        private string ErrText { get; set; }
+    class data
+    {
+        public bool connect { get; set; }
+        public byte[] data_ { get; set; }
+        public int ErrCode { get; set; }
+        public string ErrText { get; set; }
 
-        public data(bool con, int[] data__, int ErrCode_, string ErrText_) {
+        public data(bool con, byte[] data__, int ErrCode_, string ErrText_)
+        {
             connect = con; data_ = data__; ErrCode = ErrCode_; ErrText = ErrText_;
-        }        
-    }    
+        }
+    }
 }
