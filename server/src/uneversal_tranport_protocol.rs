d31a542532bdee1403138eccb715064da08a5a69
koitoTyan <34410977+koitoTyan@users.noreply.github.com>
2018-04-11 09:40:18 +0300

    Add files via upload

--- server/src/uneversal_tranport_protocol.rs
+++ server/src/uneversal_tranport_protocol.rs
@@ -8,8 +8,8 @@ use std::thread;
 use std::sync::mpsc;
 use std::fs::File;
 use std::time::Duration;
-use std::mem;
-use std::rc::Rc;
+//use std::mem;
+//use std::rc::Rc;
 
 pub struct utp{
 	port: u16,
@@ -38,44 +38,45 @@ pub struct data{
 
 
 impl data{
-	fn error(&self)->u16{
+	pub fn error(&self)->u16{
 		self.err_code
 	}
 	
-	fn buf(&self)->[u8; 9]{
+	pub fn buf(&self)->[u8; 9]{
 		self.data
 	}
-	fn drop(self){}
-    fn clone(&self)->data{ data {connect: self.connect, data: self.data, err_code: self.err_code,
+	pub fn drop(self){}
+    pub fn clone(&self)->data{ data {connect: self.connect, data: self.data, err_code: self.err_code,
         err_text: self.err_text.clone()} }
 }
     
     
 impl utp{
     
-    pub fn server_mode(mut self, connect_count: usize, ping_con: usize){
-        //максимальное количество соединений и колво пакетов после которых выполняем пинг
+    pub fn server_mode(mut self, connect_count: usize, ping_con: usize, delete: bool){
+        //максимальное количество соединений и колво пакетов после которых выполняем пинг, 
+        //удалять ли косяковых клиентов
 
         //let mut socket = UdpSocket::bind(self.local_ip.clone() + ":" + &*self.local_port.to_string());
         
-        let mut connect_count_: usize = 0;
-        
-        let mut ping_con_: usize = 0;
-        
+        let mut connect_count_: usize = 0; // колво соединений        
+        let mut ping_con_: usize = 0;      // колво сообщений
         let mut Clients: Vec<utp> = Vec::new();
-         
-        //let (sender, receiv) = mpsc::channel();
-        
-        println!("[Ожидаю пакетов..]");
+        //if self.ping() == false {println!("KOCYAK");}
+       /* println!("Начинаю пинговать..");
+        if self.ping() {
+            // что-то тут если удалось
+        } else {
+            // и тут если не удалось
+            println!("пинговка не удалась");
+        }*/
+        println!("[Запускаю операцию приёма сообщений..]");
         loop {
-            let data = self.recv();
-            
-            
-            //ping_con_ += 1;            
-            
+            if ping_con != 0 { ping_con_ += 1; }
+            let data = self.recv();        
             let mut b = false;
             if data.error() == 0 {
-                println!("[Принял сообщение!]");
+                //println!("[Принял сообщение!]");
                 for i in 0..Clients.len() {                
                     let (sender, receiv) = mpsc::channel();
                     sender.send((Clients[i].clone(), data.clone())).is_ok();
@@ -83,7 +84,7 @@ impl utp{
                     let (client, data) = match receiv.recv(){
                          Ok((A, B)) => (A, B),
                          Err(Error) => (utp{port: 1234, ip:".0.0.0".to_string(), local_port: 1234, local_ip:".0.0.0".to_string()}, data{connect: false, 
-                             data:[0u8; 9], err_code: 405, err_text: "dont send client-message on sender thread".to_string()}),
+                             data:[0u8; 9], err_code: 405, err_text: "error 'sender' thread".to_string()}),
                         };
                         if client.ip.as_str() != ".0.0.0"{
                             let r_data = client.send(data);
@@ -97,29 +98,38 @@ impl utp{
                     if self.ip == Clients[i].ip.clone() {
                         b = true;
                     }
+                    if ping_con_ >= ping_con { 
+                        ping_con_ = 0;
+                        if Clients[i].ping() == false && delete {
+                            println!("[Клиент '{}' упал, отключаю]", Clients[i].ret_addr_port().0);
+                            Clients.remove(i);
+                        } 
+                    }
                 }
                 if b {} else if connect_count_ < connect_count {
                     connect_count_ += 1;
                     println!("[О, новый клиент! Принял :3]");
                     Clients.push(self.clone()); 
+                    println!("[Клиентов всего: {}]",Clients.len());
                 }
-            } else {println!("Ошибка: {} '{}'", data.error(), data.err_text);}            
+            } else { println!("Ошибка: {} '{}'\nКлиент: {:?}", data.error(), 
+                data.err_text, self.ret_addr_port()); }            
         }
     }
     
-    fn get_addr_port(&self)->(String, u16){ (self.ip.clone(), self.port) }
+    pub fn ret_addr_port(&self)->(String, u16){ (self.ip.clone(), self.port) }
     
     pub fn set_local_ip(&mut self, ip: String){ self.local_ip = ip; }
     
-    fn ret_local_ip(&self)->String{ self.local_ip.clone() }
+    pub fn ret_local_ip(&self)->String{ self.local_ip.clone() }
     
     pub fn set_local_port(&mut self, port: u16){ self.local_port = port; }
     
-    fn ret_local_port(&self)->u16{ self.local_port }
+    pub fn ret_local_port(&self)->u16{ self.local_port }
     
-	fn ping(&self)->bool{
+    pub fn ping(&self)->bool{
 		
-		let ip: &str = &*self.ip;
+		let ip: String = self.ip.clone();
 		let port: &str = &*self.port.to_string();
 				
 		
@@ -129,50 +139,62 @@ impl utp{
 			
 
 		
-		let socket = match UdpSocket::bind(self.local_ip.clone() + &*self.local_port.to_string()){		
+		let socket = match UdpSocket::bind(self.local_ip.clone() +":" + &*self.local_port.to_string()){		
 			Ok(A) => {A},
-			Err(Er) => {return false;},	
-	        };		
-		let a = socket.connect(ip.to_string()+":"+port).is_ok();
+			Err(Er) => {
+                println!("Неудачный бинд"); return false; 
+                UdpSocket::bind(self.local_ip.clone() +":" + &*self.local_port.to_string()).unwrap()
+            },	
+	    };
+        socket.connect(ip.to_string()+":"+port).is_ok()
+		//let a = socket.connect(ip.to_string()+":"+port).is_ok();
 
-		if a == false {
+		/*if a == false {
+            println!("Неудачный коннект");
 			return false;
-                }
-	    for i in 0..3{
-            let a = socket.send(&data).is_ok();		
-            thread::sleep(Duration::from_millis(1));
+            }*/
+	    /*for i in 0..3{
+            let a = socket.send_to(&data, self.local_ip.clone() + ":" + &*self.local_port.to_string()).is_ok();	
+//            thread::sleep(Duration::from_millis(1));
         }
         
-        socket.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
+        match socket.set_read_timeout(Some(Duration::from_millis(100000))) {
+            Ok(a)=>{},
+            Err(e)=> {println!("Ошибка: '{:?}'", e); return false;},
+        }
+        println!("жду..");
         
-        let ok_q = socket.recv(&mut buf).is_ok(); // чтобы паники небыло
-        
-		if buf[8] == 101 { return true;} else { return false; } 
-		true
-	}
+        match socket.recv_from(&mut buf) {
+            Ok(A) => {
+                println!("{:?}", buf);
+                true
+            },
+            Err(err) => {*//*println!("[Ошибка при пинговке: {:?} \n(Kлиент: '{}' Порт: '{}')]", err, self.ip.clone(), port);*/ //false},
+        //} // чтобы паники небыло	
+    }
 		
 	
 	
-	fn test_connect(&self)->bool{	    
-	    
-	    let ip: &str = &*self.ip;
-	    let port: &str = &*self.port.to_string();
-	
-            let socket = match UdpSocket::bind("127.0.0.1:8080"){		
-			Ok(A) => {A},
-			Err(Er) => {return false;},	
-	    };		
-	
-	     socket.connect(ip.to_string()+":"+port).is_ok()	    
-	}
+//	fn test_connect(&self)->bool{	    
+//	    
+//	    let ip: &str = &*self.ip;
+//	    let port: &str = &*self.port.to_string();
+//	
+//            let socket = match UdpSocket::bind("127.0.0.1:8080"){		
+//			Ok(A) => {A},
+//			Err(Er) => {return false;},	
+//	    };		
+//	
+//	     socket.connect(ip.to_string()+":"+port).is_ok()	    
+//	}
 
-	fn clone(&self)->utp{
+    pub fn clone(&self)->utp{
 		utp{ port: self.port, ip: self.ip.to_string(),
             local_port: self.local_port, local_ip: self.local_ip.clone()} 
 	}
 
 	
-	fn recv(&mut self)->data{    
+    pub fn recv(&mut self)->data{    
 	    
 		//let ip: &str = &*self.ip;
 		//let port: &str = &*self.port.to_string();
@@ -183,9 +205,9 @@ impl utp{
 
 
 
-		let socket = match UdpSocket::bind(self.local_ip.clone()+":"+self.port.to_string().as_str()){		
+		let socket = match UdpSocket::bind(self.local_ip.clone()+":"+self.local_port.to_string().as_str()){		
 			Ok(A) => {A},
-			Err(Er) => {return data{connect: false, data: buf, err_code: 2, err_text: "bad server, none connect to client!".to_string()}; 
+			Err(Er) => {return data{connect: false, data: buf, err_code: 2, err_text: "bad server, none bind to client!".to_string()}; 
 						UdpSocket::bind("127.0.0.1:8080").unwrap()},	
 	        };		
 
@@ -197,35 +219,29 @@ impl utp{
         
         self.ip = ip_addr.to_string();
         
-		let a = socket.connect(self.ip.clone()+":"+self.port.clone().to_string().as_str()).is_ok();
+		/*let a = socket.connect(self.ip.clone()+":"+self.port.clone().to_string().as_str()).is_ok();
 
 		if a == false {
 			return data{connect: false, data: buf, err_code: 404, err_text: "bad connect!".to_string()}; 
-		}
-
+		} else*/ if buf[8] == 101 { return data{connect: true, data: buf, err_code: 0, err_text: "all ok".to_string()}; }
         /*
-            recv() надо переделать, чтобы он возвращал адрес (автоматом записывал
-                его в ip) 
+            recv() записывает адрес отправителя в ip 
         */
-
 		//let ok_q = socket.recv(&mut buf).is_ok(); // чтобы паники небыло
         let s:& [u8; 1] = &[buf[1]; 1];
         for i in 0..3{
             let a = socket.send(s).is_ok();		
-            thread::sleep(Duration::from_millis(1));
-        }
-		
+//            thread::sleep(Duration::from_millis(1));
+        }		
 		/*if ok_q == false { return data{connect: false, data: buf, err_code: 3, err_text: "error reading".to_string()}; }
-        */
-		if buf[8] == 101 { return data{connect: true, data: buf, err_code: 0, err_text: "all ok".to_string()}; }
-
+        */		
         data{connect: false, data: buf, err_code: 1, err_text: "bad message".to_string()}
     }
 
 
-	fn send(&self, data: data)->data{// данные, сколько ждём ответа, количество запросов
+    pub fn send(&self, data: data)->data{// данные, сколько ждём ответа, количество запросов
         
-		let ip: &str = &*self.ip;
+		let ip = self.ip.clone();
 		let port: &str = &*self.port.to_string();	
         
         
@@ -235,26 +251,32 @@ impl utp{
 			
 
 		let mut y = false;
-		let socket = match UdpSocket::bind("127.0.0.1:8081"){		
+		let socket = match UdpSocket::bind(self.local_ip.clone() + ":" + &*self.local_port.to_string()){		
 			Ok(A) => {A},
 			Err(Er) => {return data{connect: false, data: [0;9], err_code: 2, err_text: "bad server, none connect to client!".to_string()}; 
 						UdpSocket::bind("127.0.0.1:1000").unwrap()},	
 		};		
-		let a = socket.connect(ip.to_string()+":"+port).is_ok();
+		//let a = socket.connect(ip.to_string()+":"+port).is_ok();
 
-		if a == false {
-			return data{connect: false, data: [0; 9], err_code: 404, err_text: "bad connect!".to_string()}; 
-		}
+//		if a == false {
+//			return data{connect: false, data: [0; 9], err_code: 404, err_text: "bad connect!".to_string()}; 
+//		}
 		
-	    for i in 0..3{
-            let a = socket.send(&data.buf()).is_ok();		
-		    thread::sleep(Duration::from_millis(1));
-        }
-		let ok_q = socket.recv(&mut buf).is_ok(); // чтобы паники небыло
-		if ok_q == false { return data{connect: false, data: [0; 9], err_code: 3, err_text: "error reading".to_string()}; }
+	    //for i in 0..3{
+            if socket.send_to(&data.buf(), ip.clone() + ":" +port).is_ok(){
+                y = true;
+            }		
+		  //  thread::sleep(Duration::from_millis(1));
+        //}
+		/*let ok_q = socket.recv(&mut buf).is_ok(); // чтобы паники небыло
+		if ok_q == false { return data{connect: false, data: [0; 9], err_code: 3, err_text: "error reading".to_string()}; } */
 		
-		// [0] -> id message		
-		data{connect: true, data: [buf[0]; 9], err_code: 0, err_text: "all ok".to_string()}			 		
+		// [0] -> id message
+        if y {
+		  data{connect: true, data: [buf[0]; 9], err_code: 0, err_text: "all ok".to_string()}
+        } else {
+            data{connect: false, data: [0; 9], err_code: 404, err_text: "bad connect!".to_string()}
+        }			 		
 	}/*
 	fn clear(&self)->utp{
 		utp{port: 0, ip: "".to_string(), data{connect: false, data: buf, err_code: 2, err_text: "bad server, none connect to client!".to_string()} }
